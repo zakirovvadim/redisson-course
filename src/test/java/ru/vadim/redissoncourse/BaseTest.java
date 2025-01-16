f99389f39561e50bda6ba741c395e6b731161781
VadimZakirov <appolon17@gmail.com>
2025-01-16 23:51:51 +0500

    Prepare to testing examples

---
+++ src/test/java/ru/vadim/redissoncourse/BaseTest.java
@@ -0,0 +1,32 @@
+package ru.vadim.redissoncourse;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+import org.redisson.api.RedissonReactiveClient;
+import ru.vadim.redissoncourse.config.RedissonConfig;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class BaseTest {
+
+    private final RedissonConfig redissonConfig = new RedissonConfig();
+    protected RedissonReactiveClient client;
+
+    @BeforeAll
+    public void setClient() {
+        this.client = this.redissonConfig.getReactiveClient();
+    }
+
+    @AfterAll
+    public void shutdown() {
+        this.client.shutdown();
+    }
+
+    protected void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
